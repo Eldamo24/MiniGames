512cffe031f8479a9e9c9276d0d9e2c211f9a249
Damian Lobos <damianlobos24@gmail.com>
2023-12-21 10:57:08 -0300

    New folder on scripts

---
+++ Assets/Scripts/MiniGame_1/Movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float speed = 5f;
+    private float jumpForce = 3f;
+    public int lifes = 5;
+    [SerializeField]
+    private bool onFloor;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Time.timeScale = 1f;
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(-vertical, 0f, horizontal);
+        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor)
+        {
+            onFloor = false;
+            rb.velocity += jumpForce * Vector3.up;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "GamePlatform")
+        {
+            onFloor = true;
+        }
+    }
+
+}
